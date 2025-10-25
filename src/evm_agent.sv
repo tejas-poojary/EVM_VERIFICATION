06bf047c837667e5c0e06f417d360d43fa993703
Tejas poojary <tejaspoojary@feserver.mirafra.com>
2025-10-25 13:54:10 +0530

    agent_change

--- src/evm_agent.sv
+++ src/evm_agent.sv
@@ -1,1 +1,8 @@
+class evm_agent extends uvm_agent;
+ `uvm_component_utils(evm_agent);
 
+ function new(string name="evm_agent",uvm_component parent=null);
+  super.new(name,parent);
+ endfunction
+
+ endclass
