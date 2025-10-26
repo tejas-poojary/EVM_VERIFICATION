49730bb7e1503bde452755cc661cb3186e125dd0
Tejas poojary <tejaspoojary@feserver.mirafra.com>
2025-10-26 20:14:58 +0530

    driver_change

--- src/evm_driver.sv
+++ src/evm_driver.sv
@@ -11,7 +11,11 @@ class evm_driver extends uvm_driver#(evm_sequence_item);
  function void build_phase(uvm_phase phase);
   super.build_phase(phase);
   if(!uvm_config_db#(evm_interface)::get(this,"","vif",vif_drv)
-   `uvm_error(get_full_name(),"Driver didnt get interface handle");
+   `uvm_fatal(get_full_name(),"Driver didnt get interface handle");
+  if(!uvm_config_db#(event)::get(this,"","act_event",active_mon_trigger)
+   `uvm_fatal(get_full_name(),"Driver didnt get active monitor event");
+  if(!uvm_config_db#(event)::get(this,"","pas_event",passive_mon_trigger)
+   `uvm_fatal(get_full_name(),"Driver didnt get passive monitor event")
  endfunction
 
  virtual task run_phase(uvm_phase phase);
