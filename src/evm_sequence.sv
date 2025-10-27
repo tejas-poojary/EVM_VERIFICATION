5728bedf4d7d3b740a20aa9a094bd139fc40990a
Shashanka G <shashankagangadhara@feserver.mirafra.com>
2025-10-27 16:57:08 +0530

    seq and test updated

--- src/evm_sequence.sv
+++ src/evm_sequence.sv
@@ -1,1 +1,31 @@
+class evm_sequence extends uvm_sequence #(evm_sequence_item);
 
+	`uvm_object_utils(evm_sequence)
+
+	evm_sequence_item seq_item;
+
+	function new(string name = "evm_sequence");
+		super.new(name);
+	endfunction
+
+	virtual task body();
+			req = evm_sequence_item::type_id::create("req");
+			wait_for_grant();
+			req.randomize();
+			send_request(req);
+			wait_for_item_done();
+	endtask
+endclass
+
+
+class base_seq extends uvm_sequence #(apb_sequence_item); 
+	`uvm_object_utils(base_seq)
+
+	function new(string name = "base_seq");
+		super.new(name);
+	endfunction
+
+	virtual task body();
+		`uvm_do_with(req,{req.PRESETn == 1;});
+	endtask
+endclass
--- src/evm_test.sv
+++ src/evm_test.sv
@@ -1,1 +1,36 @@
+class evm_base_test extends uvm_test;
+	evm_environment env;
 
+	`uvm_component_utils(evm_base_test)
+
+	function new(string name = "evm_base_test", uvm_component parent = null);
+	super.new(name, parent);
+	endfunction
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		env = evm_environment::type_id::create("env", this);
+	endfunction
+
+	virtual function void end_of_elaboration();
+		print();
+	endfunction
+endclass
+
+
+class evm_test1 extends evm_base_test;
+	`uvm_component_utils(evm_test1)
+
+	function new(string name = "evm_test1", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction
+
+	virtual task run_phase(uvm_phase phase);
+		base_seq seq;
+		super.run_phase(phase);
+		phase.raise_objection(this, "Objection Raised");
+		seq = base_seq::type_id::create("seq");
+		seq.start(env.evm_active_agent.evm_sequencer);
+		phase.drop_objection(this, "Objection Dropped");
+	endtask
+endclass
